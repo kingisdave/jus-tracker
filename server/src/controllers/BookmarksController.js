59b470904d55fde4a2ed70bcc632c667f27b39bd
kingisdave <davidfayreal@gmail.com>
2023-01-15 23:59:43 +0100

    Updated the controller

---
+++ server/src/controllers/BookmarksController.js
@@ -0,0 +1,21 @@
+const { Bookmark } = require('../models')
+// const Sequelize = require('sequelize');
+// const Op = Sequelize.Op;
+
+module.exports = {
+  async index (req, res) {
+    try {
+      const {songId} = req.query
+      const bookmark = await Bookmark.findOne({
+        where: {
+          SongId: songId
+        }
+      })
+      res.send(bookmark)
+    } catch (error) {
+      res.status(400).send({
+        error: 'Error occurred while trying to fetch the songs'
+      })
+    }
+  }
+} 
